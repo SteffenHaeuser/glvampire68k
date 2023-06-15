836f340a5ec52bb06a30eaf1f55cc6beb322eeb9
SteffenHaeuser <37880070+SteffenHaeuser@users.noreply.github.com>
2023-06-15 12:45:29 +0200

    Initial Checkin.

---
+++ glvampire.cpp
@@ -0,0 +1,263 @@
+#include "glvampire.h"
+#include <maggie_vec.h>
+#include <map>
+#include <stack>
+std::map<int,int> *vampTextureMap = new std::map<int,int>();
+int currentTexture = -1;
+int maxVampTex = 1;
+using MatrixStack = std::stack<mat4*>;
+
+mat4 modelViewMatrix;     // aktuelle Modelview-Matrix
+mat4 projectionMatrix;    // aktuelle Projektionsmatrix
+
+// Die aktuelle Matrix, auf die Operationen angewendet werden sollen
+mat4* currentMatrix = &modelViewMatrix;
+MatrixStack matrixStack; 
+
+void glMatrixMode(int mode)
+{
+    if (mode == GL_MODELVIEW)
+    {
+        currentMatrix = &modelViewMatrix;
+    }
+    else if (mode == GL_PROJECTION)
+    {
+        currentMatrix = &projectionMatrix;
+    }
+}
+
+void glLoadIdentity()
+{
+	mat4_identity(currentMatrix);
+}
+
+void glLoadMatrix(float *matrix) {
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            currentMatrix->m[i][j] = matrix[i * 4 + j];
+        }
+    }
+}
+
+void glGetFloatv(int pname, float* params)
+{
+    if (pname == GL_MODELVIEW_MATRIX)
+    {
+        // Copy the values from the provided matrix to params
+        const mat4& matrix = *reinterpret_cast<const mat4*>(params);
+        for (int i = 0; i < 16; i++)
+        {
+            params[i] = matrix.m[i / 4][i % 4];
+        }
+    }
+}
+
+void glOrtho(float left, float right, float bottom, float top, float nearVal, float farVal) {
+    glLoadIdentity();
+
+    float tx = -(right + left) / (right - left);
+    float ty = -(top + bottom) / (top - bottom);
+    float tz = -(farVal + nearVal) / (farVal - nearVal);
+
+    currentMatrix->m[0][0] = 2.0f / (right - left);
+    currentMatrix->m[1][1] = 2.0f / (top - bottom);
+    currentMatrix->m[2][2] = -2.0f / (farVal - nearVal);
+    currentMatrix->m[3][0] = tx;
+    currentMatrix->m[3][1] = ty;
+    currentMatrix->m[3][2] = tz;
+}
+
+
+void glRotatef(float angle, float x, float y, float z) 
+{
+    glLoadIdentity();
+
+    mat4_rotateX(currentMatrix, x * angle);
+    mat4_rotateY(currentMatrix, y * angle);
+    mat4_rotateZ(currentMatrix, z * angle);
+}
+
+void glFrustum(float left, float right, float bottom, float top, float nearVal, float farVal) 
+{
+    float A = (right + left) / (right - left);
+    float B = (top + bottom) / (top - bottom);
+    float C = -(farVal + nearVal) / (farVal - nearVal);
+    float D = -(2.0f * farVal * nearVal) / (farVal - nearVal);
+
+    glLoadIdentity();
+
+    currentMatrix->m[0][0] = (2.0f * nearVal) / (right - left);
+    currentMatrix->m[1][1] = (2.0f * nearVal) / (top - bottom);
+    currentMatrix->m[2][0] = A;
+    currentMatrix->m[2][1] = B;
+    currentMatrix->m[2][2] = C;
+    currentMatrix->m[2][3] = -1.0f;
+    currentMatrix->m[3][2] = D;
+}
+
+
+
+void glPushMatrix()
+{
+    matrixStack.push(currentMatrix);
+}
+
+void glPopMatrix()
+{
+    if (!matrixStack.empty())
+    {
+        currentMatrix = matrixStack.top();
+        matrixStack.pop();
+    }
+}
+
+
+void glTranslatef(float x, float y, float z)
+{
+	mat4_translate(currentMatrix, x, y, z);
+}
+
+void glVertex3f(float x, float y, float z)
+{
+	magVertex(x,y,z);
+}
+
+void glVertex2f(float x, float y)
+{
+	magVertex(x,y,0.0f);
+}
+
+void glNormal3f(float x, float y, float z)
+{
+	magNormal(x,y,z);
+}
+
+void glVertex3fv(float *vec)
+{
+	if (vec!=0)
+	{
+		magVertex(vec[0],vec[1],vec[2]);
+	}
+}
+
+void glTexCoord2f(float x, float y)
+{
+	if (currentTexture!=-1)
+	{
+		magTexCoord(currentTexture,x,y);
+	}
+}
+
+void glBindTexture(int i, int j)
+{
+	if (i==GL_TEXTURE_2D)
+	{
+		auto num = vampTextureMap->find(j);
+		if (num!=vampTextureMap->end())
+		{
+			currentTexture = num->second;
+			magSetTexture(0,currentTexture);
+		}
+	}
+}
+
+void glTexImage2D(int i, int j, int k, int l, int m, int n, int o, int p, void *pixels)
+{
+	int texHandle = -1;
+	int pixsize = 0;
+	switch(o)
+	{
+		case GL_RGBA:
+			if (p==GL_UNSIGNED_BYTE) pixsize = 4;
+			break;
+	}
+	if (i==GL_TEXTURE_2D)
+	{
+		if (l*m==64*64) texHandle = magAllocateTexture(6);
+		else if (l*m==128*128) texHandle = magAllocateTexture(7);
+		else if (l*m==256*256) texHandle = magAllocateTexture(8);
+		if (!texHandle) return;
+		vampTextureMap->insert(std::make_pair(maxVampTex, texHandle));
+		maxVampTex++;
+		magUploadTexture(texHandle, j, pixels, 0);
+	}
+}
+
+void glDeleteTextures(int num, void *v)
+{
+	if (num==1)
+	{
+		int texnum = *((int*)(v));
+		
+		if (texnum!=0)
+		{
+			auto it = vampTextureMap->find(texnum);
+			if (it != vampTextureMap->end()) 
+			{
+				int number = it->second;
+				vampTextureMap->erase(it);
+				magFreeTexture(number);
+			}
+		}
+	}
+}
+
+void glColor4f(float r, float g, float b, float a)
+{
+	unsigned char rdec = (unsigned char)r;
+	unsigned char gdec = (unsigned char)g;
+	unsigned char bdec = (unsigned char)b;
+	unsigned char adec = (unsigned char)a;
+	
+	int color = (rdec<<16)|(gdec<<8)|(bdec)|(adec<<24);
+	
+	magColour(color);
+}
+
+void glColor3f(float x, float y, float z)
+{
+	unsigned char rdec = (unsigned char)x;
+	unsigned char gdec = (unsigned char)y;
+	unsigned char bdec = (unsigned char)z;
+	unsigned char adec = 1;
+	
+	int color = (rdec<<16)|(gdec<<8)|(bdec)|(adec<<24);
+	
+	magColour(color);	
+}
+
+void glColor4ub(int i, int j, int k, int l)
+{
+	unsigned char rdec = (unsigned char)i;
+	unsigned char gdec = (unsigned char)j;
+	unsigned char bdec = (unsigned char)k;
+	unsigned char adec = (unsigned char)l;
+
+	int color = (rdec<<16)|(gdec<<8)|(bdec)|(adec<<24);
+	
+	magColour(color);	
+}
+
+void glColor4ubv(unsigned char *col)
+{
+	if (col!=0)
+	{
+		glColor4ub(col[0],col[1],col[2],col[3]);
+	}
+}
+
+void glColor4fv(float *v)
+{
+	if (v!=0)
+	{
+		glColor4f(v[0],v[1],v[2],v[3]);
+	}
+}
+
+void glColor3fv(float *v)
+{
+	if (v!=0)
+	{
+		glColor3f(v[0],v[1],v[2]);
+	}
+}
---
+++ glvampire.h
@@ -0,0 +1,182 @@
+#ifndef GL_VAMPIRE_H
+#define GL_VAMPIRE_H
+
+#include <proto/Maggie.h>
+
+typedef enum {
+    GL_MODULATE=11,
+    GL_REPLACE
+} GLenum;
+
+// Matrix Manipulation
+
+void glPushMatrix(); // DONE
+void glPopMatrix(); // DONE
+void glLoadMatrixf(float *f); // DONE
+void glScissor(int x, int y, int w, int h);
+void glViewport(int x, int y, int w, int h);
+void glMatrixMode(int i); // DONE
+void glTranslatef(float x, float y, float z); // DONE
+void glRotatef(float i, float x, float y, float z); // DONE
+void glFrustum(int xmin, int mmax, int ymin, int ymax, int znear, int zfar); // DONE
+void glLoadIdentity(); // DONE
+void glOrtho(int i,int w, int h, int j, int k, int l); // DONE
+void glGetFloatv(int i, float *w); // Only used to acquire the World Matrix, DONE with this restriction
+
+// Colors
+
+void glClear(unsigned int i);
+void glClearColor(float i, float j, float k, float l);
+void glColor4f(float r, float g, float b, float a); // DONE
+void glColor4ub(int i, int j, int k, int l); // DONE
+void glColor3f(float x, float y, float z); // DONE
+void glColor4ubv(unsigned char *col); // DONE
+void glColor3fv(float *col); // DONE
+void glColor4fv(float *v); // DONE
+
+
+// Blending
+
+void glBlendFunc(int i, int j);
+void glAlphaFunc(int i, float j);
+void glShadeModel(int i); // switch between smooth and flat shading, not sure if a big problem if missing
+
+// Textures and Vertices
+
+void glVertex2f(float x, float y); // DONE
+void glVertex3f(float x, float y, float z); // DONE
+void glTexCoord2f(float x, float y); // DONE
+void glNormal3f(float x, float y, float z); // DONE
+void glVertex3fv(float *vec); // DONE
+void glBindTexture(int i, int j); // DONE
+void glTexImage2D(int i, int j, int k, int l, int m, int n, int o, int p, void *pixels); 
+void glDeleteTextures(int num, void *v); // DONE
+void glTexSubImage2D(int i, int j, int xoff, int yoff, int w, int h, int form, int type, void *pixels);
+
+// Stuff which is probably only used for Default settings and probably can be removed
+
+void glDepthMask(int i);
+void glCullFace(int i);
+void glDrawBuffer(int i);
+void glPolygonMode(int i, int j);
+
+// States
+
+void glEnable(int i);
+void glDisable(int i);
+
+// Handling, a lot of it can be removed
+
+int glGetError();
+void glBegin(int i);
+void glEnd();
+
+// glTexGen (might not be needed, not sure)
+
+void glTexGeni(int i, int j, int k);
+
+// Z Buffer (only used to avoid models stuck in walls)
+
+void glDepthRange(int i, int j);
+void glDepthFunc(int i);
+
+// TexEnv
+
+void glTexEnvi(int i, int j, int k);
+void glTexEnvf(int i, int j, int k);
+
+// TexParameter
+
+void glTexParameteri(int i, int j, int k);
+
+// Optional (as in REALLY REALLY optional, can be removed)
+
+void glFogi(int i, int j);
+void glFogfv(int i, float *col);
+void glReadPixels(int x, int y, int w, int h, int i, int j, void *k);
+void glFogf(int i, float j);
+char *glGetString(int i);
+
+
+#define GL_NO_ERROR 0
+#define GL_TEXTURE_2D 1
+#define GL_SRC_ALPHA 2
+#define GL_ONE_MINUS_SRC_ALPHA 3
+#define GL_GREATER 4
+#define GL_BLEND 5
+#define GL_ALPHA_TEXT 6
+#define GL_COLOR_BUFFER_BIT 7
+#define GL_ALPHA_TEST 8
+#define MGL_FLATFAN 9
+#define MGL_PERSPECTIVE_MAPPING 10
+#define GL_QUADS 13
+#define GL_SMOOTH 14
+#define GL_FLAT 15
+#define GL_TRIANGLE_STRIP 16
+#define GL_S 17
+#define GL_T 18
+#define GL_SPHERE_MAP 19
+#define GL_TEXTURE_GEN_MODE 20
+#define GL_TRIANGLE_FAN 21
+#define GL_TEXTURE_GEN_S 22
+#define GL_TEXTURE_GEN_T 23
+#define GL_FOG 24
+#define GL_RGBA 25
+#define GL_UNSIGNED_BYTE 26
+#define GL_TEXTURE_MIN_FILTER 27
+#define GL_TEXTURE_MAG_FILTER 28
+#define GL_NEAREST_MIPMAP_NEAREST 29
+#define GL_LINEAR_MIPMAP_NEAREST 30
+#define GL_NEAREST_MIPMAP_LINEAR 31
+#define GL_LINEAR_MIPMAP_LINEAR 32
+#define GL_TEXTURE_ENV 33
+#define GL_TEXTURE_ENV_MODE 34
+#define GL_NEAREST 35
+#define GL_LINEAR 36
+#define GL_ONE 37
+#define GL_DEPTH_TEST 38
+#define GL_CULL_FACE 39
+#define GL_FRONT 40
+#define GL_BACK 41
+#define GL_PROJECTION 42
+#define GL_MODELVIEW 43
+#define GL_DECAL 44
+#define GL_DITHER 45
+#define GL_RENDERER 46
+#define GL_VERSION 47
+#define GL_EXTENSIONS 48
+#define GL_UNPACK_ALIGNMENT 49
+#define GL_PACK_ALIGNMENT 50
+#define GL_FRONT_AND_BACK 51
+#define GL_FILL 52
+#define GL_TEXTURE_WRAP_S 53
+#define GL_TEXTURE_WRAP_T 54
+#define GL_REPEAT 55
+#define GL_RGB 56
+#define GL_POLYGON 57
+#define GL_LINE_STRIP 58
+#define GL_ZERO 59
+#define GL_SRC_COLOR 60
+#define GL_INTENSITY8 61
+#define GL_LUMINANCE8 62
+#define MGL_Z_OFFSET 63
+#define GL_FOG_END 64
+#define GL_FOG_START 65
+#define GL_FOG_DENSITY 66
+#define GL_VENDOR 67
+#define GL_TRUE 68
+#define GL_FALSE 69
+#define GL_GEQUAL 70
+#define GL_DEPTH_BUFFER_BIT 71
+#define GL_LEQUAL 72
+#define GL_FOG_COLOR 73
+#define GL_FOG_MODE 74
+#define GL_EXP 75
+#define GL_EXP2 76
+#define GL_MODELVIEW_MATRIX 77
+#define GL_SCISSOR_TEST 78
+
+#define GLfloat float
+#define GLint int
+#define GLdouble double
+#endif
